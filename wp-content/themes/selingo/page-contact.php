062f5a39f5dce08e0698792485d119e5103db7cc
jasonglisson <jason.glisson@gmail.com>
2016-02-01 13:02:14 -0500

    Fixed column widths

--- wp-content/themes/selingo/page-contact.php
+++ wp-content/themes/selingo/page-contact.php
@@ -10,7 +10,7 @@ Template Name: Contact
 	
 		<div id="inner-content" class="row">
 	
-		    <main id="main" class="large-8 medium-8 columns" role="main">
+		    <main id="main" class="large-12 medium-12 columns" role="main">
 				
 				<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
